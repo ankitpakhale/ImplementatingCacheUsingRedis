f4aaa2181bdf5ef67feacac050a2f8922078241c
ankitpakhale <akp3067@gmail.com>
2022-08-20 23:07:15 +0530

    Added:

---
+++ README.md
@@ -0,0 +1,5 @@
+# Integrating Redis in Django | Use Caching in Django
+
+Connect Redis Server (CACHE SECTION) in settings.py
+
+Once data came from db it will set in cache and than again we'll search for that particular data it will give result from cache memory
--- redis_app/views.py
+++ redis_app/views.py
@@ -1,19 +1,42 @@
 from django.shortcuts import render
 from .models import *
+from django.conf import settings
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from django.views.decorators.cache import cache_page
+from django.core.cache import cache
 # Create your views here.
 
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
+
 def get_recipe(filter_recipe = None):
-    recipes = Recipe.objects.all()
+    if filter_recipe:
+        print("Data is coming from DATABASE")
+        recipes = Recipe.objects.filter(name__contains = filter_recipe)
+    else:      
+        recipes = Recipe.objects.all()
     return recipes
 
 def home(request):
     filter_recipe = request.GET.get('recipe')
-    recipe = get_recipe()
+    if cache.get(filter_recipe):
+        print("Data is coming from CACHE")
+        recipe = cache.get(filter_recipe)   
+    else:   
+        if filter_recipe:
+            recipe = get_recipe(filter_recipe)
+            cache.set(filter_recipe, recipe)
+        else: 
+            recipe = get_recipe()
     context = {'recipe': recipe}
     return render(request, 'home.html', context)
     
 def show(request, id):
-    recipe = Recipe.objects.get(id=id)
+    if cache.get(id):
+        recipe = Recipe.objects.get(id=id)
+        print("SHOW Data CACHE")
+    else:
+        print("SHOW Data DATABASE")
+        recipe = Recipe.objects.get(id=id)
+        cache.set(id, recipe)
     context = {'recipe': recipe}
     return render(request, 'show.html', context)
-    
